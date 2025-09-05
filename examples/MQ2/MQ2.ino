dd3d6b3681448dbcaf4b96dabf2905f9b7b3bcfb
egarDC <eloy.ingenieria@digitalcodesign.com>
2025-09-05 09:00:41 +0100

    Update MQ2.ino

--- examples/MQ2/MQ2.ino
+++ examples/MQ2/MQ2.ino
@@ -17,9 +17,9 @@
 
 
    Autor: Digital Codesign
-   Version: 1.0.0
+   Version: 1.1.0
    Fecha de creación: Septiembre de 2024
-   Fecha de version: Septiembre de 2024
+   Fecha de version: Septiembre de 2025
    Repositorio: https://github.com/DigitalCodesign/MentorBit-MQ
    Descripcion:
       Este sketch es un ejemplo de como medir el valor de hidrogeno (h2) en ppm usando un
@@ -48,7 +48,7 @@
     
 
 */
-MentorBitMQ2 mq2(MQ_PIN, mq2.h2);
+MentorBitMQ2 mq2( mq2.h2, MQ_PIN);
 
 
 void setup(){
